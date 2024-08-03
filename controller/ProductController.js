7f6fc94e5abefbe1fcc84c7ef4ab614a92012890
Lakshmanan <iamlakshmananeee@gmail.com>
2024-08-03 15:25:38 +0530

    changed

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -16,7 +16,7 @@ exports.set =async(req,res)=>{
 exports.get = async (req, res) => {
     try {
         const products = await Product.find();
-        res.send("succes");
+        res.json(products);
     } catch (error) {
         res.status(500).json({ message: 'Server error' });
     }
